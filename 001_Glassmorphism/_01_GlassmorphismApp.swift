c42aa31cff7ddc9491780d79d979b712896167fa
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-09-17 23:15:39 +0300

    Initial Commit

---
+++ 001_Glassmorphism/_01_GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  _01_GlassmorphismApp.swift
+//  001_Glassmorphism
+//
+//  Created by nikita on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _01_GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
